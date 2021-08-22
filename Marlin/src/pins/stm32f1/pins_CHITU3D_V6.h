20c488787d0f8ef48279a9cd6eaa7819e7e51fcc
Felipe Olivaes <felipeolivaes@DEV-CW002762.local>
2021-08-22 15:51:01 -0300

    Modificacoes config tronxy xy2-pro - config dudu

--- Marlin/src/pins/stm32f1/pins_CHITU3D_V6.h
+++ Marlin/src/pins/stm32f1/pins_CHITU3D_V6.h
@@ -56,7 +56,8 @@
 //
 #define X_STOP_PIN                          PG10
 #define Y_STOP_PIN                          PA12
-#define Z_STOP_PIN                          PG9
+//FOI TROCADO, ANTES PG9
+#define Z_STOP_PIN                          PF13
 
 //
 // Steppers
@@ -116,7 +117,7 @@
   #define FIL_RUNOUT_PIN                    PA15  // MT_DET
 #endif
 #ifndef FIL_RUNOUT2_PIN
-  #define FIL_RUNOUT2_PIN                   PF13
+  #define FIL_RUNOUT2_PIN                   PG9
 #endif
 
 // SPI Flash
